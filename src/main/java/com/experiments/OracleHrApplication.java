db4e39b36b7026f784add60870a76bd6b3af9a93
Shankar <sankar.tulasi@gmail.com>
2020-09-06 19:33:34 +0530

    first commit

---
+++ src/main/java/com/experiments/OracleHrApplication.java
@@ -0,0 +1,13 @@
+package com.experiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleHrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experiments/OracleHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
