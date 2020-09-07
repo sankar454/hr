7ad399a2476ce04c4b075c63a8be81cf18310b04
Shankar <sankar.tulasi@gmail.com>
2020-09-07 23:08:33 +0530

    Added Employee and removed @EnableBatchProcessing in OracleHrApplication

---
+++ src/main/java/com/experiments/entity/Employee.java
@@ -0,0 +1,134 @@
+package com.experiments.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "EMPLOYEES")
+public class Employee {
+
+	@Id
+	@Column(name = "EMPLOYEE_ID")
+	private Integer employeeId;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String phoneNumber;
+	
+	@Temporal(TemporalType.DATE)
+	private Date hireDate;
+	
+	private String jobId;
+	
+	private Double salary;
+	
+	private Double commissionPct;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "MANAGER_ID")
+	private Employee manager;
+	
+	private Integer departmentId;
+
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Double getCommissionPct() {
+		return commissionPct;
+	}
+
+	public void setCommissionPct(Double commissionPct) {
+		this.commissionPct = commissionPct;
+	}
+	
+	public Integer getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Integer departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public Employee getManager() {
+		return manager;
+	}
+
+	public void setManager(Employee manager) {
+		this.manager = manager;
+	}
+
+}
--- src/main/java/com/experiments/entity/Employeee.java
+++
@@ -1,8 +0,0 @@
-package com.experiments.entity;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Employeee {
-
-}
